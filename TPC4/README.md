2b09826813e745251787d93007cde6444c35fa01
Tiago5Carneiro <tiago.carneiro.email@gmail.com>
2025-03-04 16:42:23 +0000

    Started tpc4, makes all tokens, needs rethinking ofr ':'

---
+++ TPC4/README.md
@@ -0,0 +1,19 @@
+# Análise de um dataset de obras musicais 
+
+## Autor
+- Nome : Tiago André Leça Carneiro
+- Número : A93207
+
+<img src = "../media/722ff411-84c8-44a3-b34d-b639022e9b0e.jpg" alt = "eu" style="text-align = center;" width = "200">
+
+## Resumo
+
+### Requisitos
+
+
+### Output
+
+
+## Lista de Resultados
+
+- [tpc4.py](tpc4.py)
---
+++ TPC4/tpc4.py
@@ -0,0 +1,75 @@
+import re
+import ply.lex as lex
+
+# Reserved words
+reserved = {
+    'select': 'SELECT',
+    'where': 'WHERE',
+    'limit': 'LIMIT'
+}
+
+# List of token names
+tokens = [
+    'IDENTIFIER',
+    'NUMBER',
+    'STRING',
+    'DOT',
+    'EQUALS',
+    'QMARK',
+    'LBRACE',
+    'RBRACE'
+] + list(reserved.values())
+
+# Regular expression rules for simple tokens
+t_DOT = r'\.'
+t_EQUALS = r'='
+t_QMARK = r'\?'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+
+# 
+def t_IDENTIFIER(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'IDENTIFIER')
+    return t
+
+# Define a rule so we can track line numbers
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_STRING(t):
+    r"\"[^']*\""
+    t.value = t.value[1:-1]
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+t_ignore = ' \t'
+
+# Build the lexer
+lexer = lex.lex()
+
+# Test it out
+data = open('input/test.file').read()
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
